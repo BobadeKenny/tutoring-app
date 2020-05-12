106b011bd1469fde9fd5c94726590ab57830f920
Kenny Bobade <kennygbeminiyi@gmail.com>
2020-05-12 23:34:55 +0100

    misc

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var mongoose = require("mongoose")
 var express = require("express")
+const path = require('path')
+const User = require('./models/user')
+const routes = require('./routes/auth.js');
+ 
+require("dotenv").config();
 var app = express()
 const authRoutes = require("./routes/auth");
 app.use(express.json());
@@ -14,20 +19,7 @@ if (port == null || port == ""){
   port = 3000
 }
 
-let url = "mongodb+srv://KennyB:aC36WwMoTjs6QXdR@cluster0-iryjr.gcp.mongodb.net/test?retryWrites=true&w=majority"
-
-mongoose
-  .connect(
-  url,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(result => {
-    console.log("Database connected");
-    app.listen(port);
-  })
-  .catch(err => console.log(err));
-
-/*let url = "mongodb://localhost:27017/myapp"
+/*let url = process.env.MyDb
 
 mongoose
   .connect(
@@ -39,3 +31,16 @@ mongoose
     app.listen(port);
   })
   .catch(err => console.log(err));*/
+
+let url = "mongodb://localhost:27017/myapp"
+
+mongoose
+  .connect(
+  url,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(result => {
+    console.log("Database connected");
+    app.listen(port);
+  })
+  .catch(err => console.log(err));

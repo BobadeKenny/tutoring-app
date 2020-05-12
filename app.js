eb18d5d0ecc51184d6113b0b4b0e0bc08531825a
Kenny Bobade <kennygbeminiyi@gmail.com>
2020-05-13 00:10:04 +0100

    mmm

--- app.js
+++ app.js
@@ -19,20 +19,7 @@ if (port == null || port == ""){
   port = 3000
 }
 
-/*let url = process.env.MyDb
-
-mongoose
-  .connect(
-  url,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(result => {
-    console.log("Database connected");
-    app.listen(port);
-  })
-  .catch(err => console.log(err));*/
-
-let url = "mongodb://localhost:27017/myapp"
+let url = process.env.MyDb
 
 mongoose
   .connect(
@@ -44,3 +31,16 @@ mongoose
     app.listen(port);
   })
   .catch(err => console.log(err));
+
+/*let url = "mongodb://localhost:27017/myapp"
+
+mongoose
+  .connect(
+  url,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(result => {
+    console.log("Database connected");
+    app.listen(port);
+  })
+  .catch(err => console.log(err));*/
